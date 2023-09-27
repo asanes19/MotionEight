72453c314b6ccf8b4e39a6f99c8ae209119d2639
unknown <abdelrahmansherif1930@gmail.com>
2023-09-28 01:43:05 +0400

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,78 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    })
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    })
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
 }
-
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
+    const navMenu = document.getElementById("nav-menu");
     // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu');
+    navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
-    const header = document.getElementById('header')
+    const header = document.getElementById("header");
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 50) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
-
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
-
-
-
+window.addEventListener("scroll", scrollUp);
 
 // type writer effect
 var typed = new Typed(".second-span", {
     strings: ["Brands.", "Stories.", "Memories."],
     typeSpeed: 150,
     backSpeed: 150,
-    loop: true
-})
+    loop: true,
+});
 
 //fade in banner animation
 
 // Initialize ScrollReveal
 const sr = ScrollReveal({
-    origin: 'left', // Animation from the left
-    distance: '20px', // Distance to slide
+    origin: "left", // Animation from the left
+    distance: "20px", // Distance to slide
     duration: 800, // Animation duration (in milliseconds)
     delay: 200, // Delay before animation starts (in milliseconds)
     reset: false, // Resets the animation on exit
 });
 
 // Define the elements you want to animate for the banner
-const titleElement = document.querySelector('.bannerTitle');
-const paragraphElement = document.querySelector('.bannerParagraph');
-const listElement = document.querySelector('.bannerList');
+const titleElement = document.querySelector(".bannerTitle");
+const paragraphElement = document.querySelector(".bannerParagraph");
+const listElement = document.querySelector(".bannerList");
 
 // Define the elements you want to animate for the slider-content
-const sliderContents = document.querySelectorAll('.slider-content');
+const sliderContents = document.querySelectorAll(".slider-content");
 
 // Add the animations to the elements for the banner
 sr.reveal(titleElement);
@@ -87,7 +84,7 @@ sliderContents.forEach((content, index) => {
     sr.reveal(content, { delay: 400 + index * 200 }); // Add delay for staggered animation
 });
 
-  /*=============== scroll reveal fade in ===============*/
+/*=============== scroll reveal fade in ===============*/
 const srAuto = ScrollReveal({
     origin: "left",
     distance: "70px",
@@ -110,50 +107,30 @@ animateElements.forEach((element) => {
 });
 
 srAuto.reveal(".animate-element", { interval: 500 });
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    const apartmentSections = document.querySelectorAll(".servicesSection");
+    const mySwiper = new Swiper(".swiper-container", {
+        // Customize Swiper options here
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        autoplay: {
+            delay: 5000, // Auto-advance every 5 seconds (5000 milliseconds)
+        },
+    });
 
-    apartmentSections.forEach(function (apartmentSection, index) {
-        let currentSlide = 0;
-        const images = apartmentSection.querySelector(".image-slider").querySelectorAll("img");
-        const titles = apartmentSection.querySelectorAll(".slider-title");
-        const descriptions = apartmentSection.querySelectorAll(".slider-content p");
-        const buttons = apartmentSection.querySelectorAll(".slider-button");
-        const totalSlides = images.length;
+    // Find all video elements in the slider
+    const videoElements = document.querySelectorAll(".swiper-slide-video");
 
-        function showSlide(slideIndex) {
-            images.forEach((image, idx) => {
-                if (idx === slideIndex) {
-                    image.style.opacity = 1;
-                    titles[idx].style.opacity = 1;
-                    descriptions[idx].style.opacity = 1;
-                    buttons[idx].style.opacity = 1;
-                    sliderContents[idx].classList.add("active"); // Add the "active" class to the content div
-                } else {
-                    image.style.opacity = 0;
-                    titles[idx].style.opacity = 0;
-                    descriptions[idx].style.opacity = 0;
-                    buttons[idx].style.opacity = 0;
-                    sliderContents[idx].classList.remove("active"); // Remove the "active" class
-                }
-            });
-        }
-        
-        
-
-        function nextSlide() {
-            currentSlide = (currentSlide + 1) % totalSlides;
-            showSlide(currentSlide);
-        }
-
-        function autoSlide() {
-            nextSlide();
-            setTimeout(autoSlide, 3000); // Change slide every 5 seconds
-        }
-
-        showSlide(currentSlide);
-        autoSlide();
+    // Loop all videos continuously
+    videoElements.forEach((video) => {
+        video.addEventListener("ended", function () {
+            // Reset video playback to the beginning when it ends
+            video.currentTime = 0;
+            video.play();
+        });
     });
 });
