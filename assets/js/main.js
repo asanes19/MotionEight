d4d8c49a569d148cd6aed0cf3fc058cbf16d0932
unknown <abdelrahmansherif1930@gmail.com>
2023-10-11 05:49:20 +0400

    vid page

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,9 +104,9 @@ animatePartnerLogos();
 document.addEventListener("DOMContentLoaded", function () {
     // Initialize Swiper
     const mySwiper = new Swiper(".swiper-container", {
-        autoplay: {
-            delay: 5000,
-        },
+        // autoplay: {
+        //     delay: 5000,
+        // },
         speed: 1000,
         slidesPerView: 1,
         loop: true,
@@ -115,3 +115,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
+// JavaScript for opening and closing video popup
+document.addEventListener("DOMContentLoaded", function () {
+    const playIcons = document.querySelectorAll(".play-icon");
+    const videoPopups = document.querySelectorAll(".video-popup");
+    const closeButtons = document.querySelectorAll(".close-popup");
+
+    playIcons.forEach(function (playIcon, index) {
+        const videoPopup = videoPopups[index];
+        const closeButton = closeButtons[index];
+
+        // Open video popup when the play icon is clicked
+        playIcon.addEventListener("click", function () {
+            videoPopup.style.display = "block";
+        });
+
+        // Close video popup when the close button is clicked
+        closeButton.addEventListener("click", function () {
+            videoPopup.style.display = "none";
+        });
+    });
+});
+
+// JavaScript for toggling the 'active' class on click
+document.addEventListener("DOMContentLoaded", function () {
+    const eachTypes = document.querySelectorAll(".eachType");
+
+    eachTypes.forEach(function (eachType) {
+        eachType.addEventListener("click", function () {
+            eachType.classList.toggle("active");
+        });
+    });
+});
